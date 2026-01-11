2b76f4994c3ed53a87f892840eb8edcd3efcd6d1
Bartosz Wiśniewski <bartoszwisniewski91@gmail.com>
2026-01-11 21:23:13 +0000

    Initial commit - dodanie aplikacji i Jenkinsfile

--- push_to_github.sh
+++ push_to_github.sh
@@ -5,7 +5,7 @@ git add .
 git commit -m "Initial commit: Node.js app with tests"
 
 # 4. Połączenie z GitHubem (ZASTĄP PONIŻSZY LINK SWOIM)
-# git remote add origin https://github.com/bartoszwisniewski91/Lekcja_25_CI_cz1.git
+git remote add origin https://github.com/bartoszwisniewski91/Lekcja_25_CI_cz1.git
 
 # 5. Wysłanie kodu
 git branch -M main
