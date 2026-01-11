a4f48e29844ef2fb26bf07e24e95fc0b4ad78e33
bartoszwisniewski <127435553+bartoszwisniewski91@users.noreply.github.com>
2026-01-12 00:09:10 +0100

    Update push_to_github.sh

--- push_to_github.sh
+++ push_to_github.sh
@@ -3,10 +3,6 @@ git init
 echo "node_modules/" > .gitignore
 git add .
 git commit -m "Initial commit: Node.js app with tests"
-
-# 4. Połączenie z GitHubem (ZASTĄP PONIŻSZY LINK SWOIM)
 git remote add origin https://github.com/bartoszwisniewski91/Lekcja_25_CI_cz1.git
-
-# 5. Wysłanie kodu
 git branch -M main
 git push -u origin main
